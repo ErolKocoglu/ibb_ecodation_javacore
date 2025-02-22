f1d3fbc6c5468b535d3b4355bc96fbc7bcc452a5
ErolKocoglu <erolkocoglu44@gmail.com>
2025-02-22 15:14:09 +0300

    string operations

--- src/main/java/com/erolkocoglu/exercises/User.java
+++ src/main/java/com/erolkocoglu/exercises/User.java
@@ -1,4 +1,37 @@
 package com.erolkocoglu.exercises;
 
 public class User {
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        User u = new User();
+        u.setName("Erol");
+        u.setSurname("Koçoğlu");
+
+        StringBuilder surnameToPrint = new StringBuilder(u.getSurname());
+        int length = surnameToPrint.length();
+        if(surnameToPrint.length()>3){
+            surnameToPrint = new StringBuilder(surnameToPrint.substring(0, 3));
+            for(int i=3;i<length;i++){
+                surnameToPrint.append("*");
+            }
+
+        }
+
+        System.out.println("Surname: "+surnameToPrint);
+    }
 }
